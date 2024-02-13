987bc042367c9939f4ec5ea295f40f74494dc76b
l.mousques <105661530+Svenald@users.noreply.github.com>
2024-02-13 10:11:20 -0500

    update_degats_et_soins

--- PO1_Encapsulation/Form1.cs
+++ PO1_Encapsulation/Form1.cs
@@ -17,31 +17,40 @@ namespace PO1_Encapsulation
 
         private void ActualiserLaberls()
         {
-            lbl_pointsVieJoueur.Text = "PV : " + joueur.PointsDeVie;
-            lbl_pointsVieDragon.Text = "PV : " + dragon.PointsDeVie;
-            lbl_puissanceAttaqueJoueur.Text = "Pts de Puissance : " + joueur.PointsDePuissance;
-            lbl_puissanceAttaqueDragon.Text = "Pts de puissance : " + dragon.PointsDePuissance;
+            lbl_pointsVieJoueur.Text = "PV : " + joueur.PointsDeVie.ToString();
+            lbl_pointsVieDragon.Text = "PV : " + dragon.PointsDeVie.ToString();
+            lbl_puissanceAttaqueJoueur.Text = "Pts de Puissance : " + joueur.PointsDePuissance.ToString();
+            lbl_puissanceAttaqueDragon.Text = "Pts de puissance : " + dragon.PointsDePuissance.ToString();
         }
 
         private void btn_attaquer_Click(object sender, EventArgs e)
         {
             dragon.PointsDeVie -= joueur.PointsDePuissance;
+            TourDragon();
             ActualiserLaberls();
         }
 
         private void btn_seSoigner_Click(object sender, EventArgs e)
         {
+            joueur.PointsDeVie += 5;
+            TourDragon();
             ActualiserLaberls();
         }
 
         private void btn_puissanceAttaque_Click(object sender, EventArgs e)
         {
+            joueur.PointsDePuissance++;
+            TourDragon();
             ActualiserLaberls();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             ActualiserLaberls();
+        }
+        private void TourDragon()
+        {
+            joueur.PointsDeVie -= dragon.PointsDePuissance;
         }
     }
 }
